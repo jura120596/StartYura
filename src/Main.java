bc7864bd56d7770293102ee562f143f7bd026d72
Yury Mironov <jura120596@gmail.com>
2026-02-10 18:04:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 100;
+        int y = 100;
+        int z = 100;
+        int sum = x + y;
+        System.out.println(sum);
+        System.out.println(y + z);
+        System.out.println("hello world");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("hello " + name);
+    }
+}
