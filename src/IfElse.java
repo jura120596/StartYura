1fa247ec4c730f880aa61bda90a4ae237ae69f97
YuryMironovHP <jura120596@gmail.com>
2026-02-17 17:47:55 +0300

    add if else example

---
+++ src/IfElse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class IfElse {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите ответ:");
+//        int x = scanner.nextInt();
+        String answer = scanner.nextLine();
+//        boolean condition = x < 0 || x > 10;
+        boolean condition = answer.equals( "ДА");
+        if (condition) {
+            System.out.println("Вы меня увидите, если condition = TRUE");
+        } else {
+            if (answer.equals("НЕТ")) {
+                System.out.println("Вы ввели \"НЕТ\"");
+            } else {
+                System.out.println("Вы ввели что-то непонятное");
+            }
+        }
+        System.out.println("Меня вы видите ВСЕГДА");
+    }
+}
